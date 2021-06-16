0d0424b91c08b4174aff13c77933e214ad88f9e0
emmed <med_em@hotmail.com>
2021-06-16 11:01:19 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react' 
+import ImageCard from './components/ImageCard'
+
 
 function App() {
+  const API_URL = 'https://picsum.photos/v2/list'
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState()
+  
+useEffect(() => {
+  setIsLoading(true)
+  fetch(API_URL)
+  .then(res => res.json())
+  .then(data => {
+    setImages(data)
+    setIsLoading(false)
+    console.log("Author: " + data[1].author, "+ id: " +  data[1].id)
+    // console.log("binnen img",images[1].author)
+  })
+  .catch(error => setError(error))
+}, [])
+
+ 
+if(isLoading) return <p>Data is loading...</p>
+
+if (error) return <h2>Error..</h2>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+       
+         {images.map(image => (
+            //  <div key={index}>
+            //  <div>{image.id}</div>
+            //  <div>{image.author}</div>
+            //  <img src={image.download_url} alt="img" />
+            //  </div>
+           
+        <ImageCard key={image.id} image={image}/>
+         ))}
+      </div>
     </div>
   );
 }
